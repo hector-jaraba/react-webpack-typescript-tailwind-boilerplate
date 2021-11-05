adca30d146974a2f1073e125633930b4bfd3b805
Héctor Jaraba <hector.jaraba@mimacom.com>
2021-11-05 11:47:39 +0100

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './styles.scss'
+const App: React.FC = () => {
+    return (
+        <div className="wrapper">
+            <h1>tardeOapp</h1>
+            <h2>React 17 and TypeScript 4!🚀</h2>
+        </div>
+    )
+}
+export default App
